e492faae84ebab5289a079f87812790dfdd19c08
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-07 10:56:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-c17-l5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-jb7h6c)
---
+++ src/router.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import App from './App';
+import Homepage from './pages/Homepage/Homepage';
+import Profile from './pages/Profile/Profile';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: '/profile',
+        element: <Profile />,
+        caseSensitive: true,
+      },
+    ],
+  },
+]);
